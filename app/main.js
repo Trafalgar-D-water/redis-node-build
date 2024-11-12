0e55e8f0f44e47e542a5d11697f83db974bd7bc1
priyanshu <patnayakpriyanshu@gmail.com>
2024-11-12 22:43:38 +0530

    set-get

--- app/main.js
+++ app/main.js
@@ -1,28 +1,40 @@
 const net = require("net");
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+
+const STORAGE = {};
 
 // Create server
 const server = net.createServer((connection) => {
   console.log('Client connected');
 
-  // Handle incoming data
   connection.on('data', (data) => {
-      // connection.write('+PONG\r\n'); 
-      const commnad = Buffer.from(data).toString().split("\r\n");
-
-      if(commnad[2] == 'ECHO'){
-        const str = commnad[4];
-        const l = str.length;
-        return connection.write("$" + l + "\r\n" + str + "\r\n");
-      }
-      connection.write('+PONG\r\n')
-  });
-
-  connection.on('end', () => {
-    console.log('Client disconnected');
-  });
+    console.log('   Data received: ' + data.toString());
+    const msg = data.toString().split('\r\n')
+    const command = msg[2]
+    const arg = msg[4]
+    switch (command) {
+        case 'echo':
+            connection.write(`$${arg.length}\r\n${arg}\r\n`)
+            break
+        case 'ping':
+            connection.write('+PONG\r\n')
+            break
+        case 'SET':
+            STORAGE[msg[4]] = msg[6]
+            connection.write('+OK\r\n')
+            break
+        case 'GET':
+            connection.write(`$${STORAGE[msg[4]].length}\r\n${STORAGE[msg[4]]}\r\n` || '$-1\r\n')
+            break
+        default:
+            1
+            break
+    }
+});
+// Handle end
+connection.on('end', () => {
+    console.log('   Client disconnected');
+});
 
   // Optional: Handle errors
   connection.on('error', (err) => {
