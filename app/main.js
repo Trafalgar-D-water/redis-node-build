a0d6dc592df7b7cac09246e288604d5d5fb146df
priyanshu <patnayakpriyanshu@gmail.com>
2024-11-12 18:39:10 +0530

    ping

--- app/main.js
+++ app/main.js
@@ -3,9 +3,25 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
+// Create server
 const server = net.createServer((connection) => {
-    connection.write('+PONG\r\n')
+  console.log('Client connected');
+
+  // Handle incoming data
+  connection.on('data', (data) => {
+    if (data.toString() === '*1\r\n$4\r\nPING\r\n') {
+      connection.write('+PONG\r\n'); 
+    }
+  });
+
+  connection.on('end', () => {
+    console.log('Client disconnected');
+  });
+
+  // Optional: Handle errors
+  connection.on('error', (err) => {
+    console.error('Error: ', err);
+  });
 });
 
 server.listen(6379, "127.0.0.1");
